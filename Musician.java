73b6a180024031291f5688d22a41f7ce2368626a
Tom Meier <meier.tom@gmail.com>
2022-08-21 11:00:12 -0700

    Add files via upload

---
+++ Musician.java
@@ -0,0 +1,22 @@
+
+public class Musician {
+
+	private String name;
+	private String lyric;
+	
+	public Musician(String name, String lyric) {
+		this.name = name;
+		this.lyric = lyric;
+		
+	
+		
+		
+	}
+	
+	public void sing() {
+		System.out.println("My name is " + name + " and I sing " +this.lyric);
+		
+	}
+	
+}
+
