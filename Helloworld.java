75bcad0f5ee472bd94ae2622e4ff8a03d9363f59
Tom Meier <meier.tom@gmail.com>
2022-08-21 11:13:22 -0700

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+
+public class Helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+		
+		Musician Slash = new Musician("Slash", "WTTJ");
+		
+		Slash.sing();
+	}
+
+}
